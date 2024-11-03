d2641f1848f811231b42b9eabfbebde62321616b
Andrew Grosser <dioptre@gmail.com>
2024-11-03 01:45:22 -0800

    working serial

--- src/main.c
+++ src/main.c
@@ -11,7 +11,6 @@
 #define CAPTURE_DEPTH 32
 #define LOG_DEPTH 2048
 
-
 const uint32_t LED_PIN = 25;
 const uint32_t fs = 10000;
 const uint32_t BENCHMARK_PIN = 2;
@@ -22,8 +21,7 @@ uint16_t cap0[CAPTURE_DEPTH] = {0};
 uint16_t cap1[CAPTURE_DEPTH] = {0};
 // Processing buffer
 uint16_t buffer[CAPTURE_DEPTH] = {0};
-//fix16_t buffer[CAPTURE_DEPTH] = {0};
-
+// fix16_t buffer[CAPTURE_DEPTH] = {0};
 
 int32_t log0[LOG_DEPTH] = {0};
 int32_t log1[LOG_DEPTH] = {0};
@@ -31,7 +29,6 @@ int32_t *read_logger = log0;
 uint32_t log_index = 0;
 static bool log_ping = true;
 void core1_main();
-
 
 //--------------------------------------------------------------------+
 // MIDI Stack callback (not needed right now)
@@ -55,22 +52,16 @@ void tud_resume_cb(void)
 {
 }
 
-
 int main()
 {
     int midiBufferSize = 10;
-    //midi_message_t midiMsgs[midiBufferSize];
+    // midi_message_t midiMsgs[midiBufferSize];
     int numberOfMessages;
     uint16_t *dma_buffer = cap1;
 
     stdio_init_all();
     board_init();
     tusb_init();
-
-    // while (!tud_cdc_connected()) {
-    //     sleep_ms(10);  // Wait until USB CDC is connected
-    // }
-
 
     int32_t *write_logger = log1;
 
@@ -92,11 +83,11 @@ int main()
 
     adc_select_input(CAPTURE_CHANNEL);
     adc_fifo_setup(
-        true,    // Write each completed conversion to the sample FIFO
-        true,    // Enable DMA data request (DREQ)
-        1,       // DREQ (and IRQ) asserted when at least 1 sample present
-        false,   // We won't see the ERR bit because of 8 bit reads; disable.
-        false    // No need to shift since we want 16 bits
+        true,  // Write each completed conversion to the sample FIFO
+        true,  // Enable DMA data request (DREQ)
+        1,     // DREQ (and IRQ) asserted when at least 1 sample present
+        false, // We won't see the ERR bit because of 8 bit reads; disable.
+        false  // No need to shift since we want 16 bits
     );
     adc_set_clkdiv(4800); /* Scale down to 10khz */
 
@@ -118,14 +109,13 @@ int main()
     channel_config_set_dreq(&cfg, DREQ_ADC);
 
     dma_channel_configure(dma_chan, &cfg,
-        cap0,    // dst
-        &adc_hw->fifo,  // src
-        CAPTURE_DEPTH,  // transfer count
-        true            // start immediately
+                          cap0,          // dst
+                          &adc_hw->fifo, // src
+                          CAPTURE_DEPTH, // transfer count
+                          true           // start immediately
     );
 
     adc_run(true);
-
 
     multicore_launch_core1(core1_main);
 
@@ -141,10 +131,10 @@ int main()
             dma_channel_configure(
                 dma_chan,
                 &cfg,
-                cap1,           // dst
-                &adc_hw->fifo,  // src
-                CAPTURE_DEPTH,  // transfer count
-                true            // start immediately
+                cap1,          // dst
+                &adc_hw->fifo, // src
+                CAPTURE_DEPTH, // transfer count
+                true           // start immediately
             );
             dma_buffer = cap0;
         }
@@ -153,10 +143,10 @@ int main()
             dma_channel_configure(
                 dma_chan,
                 &cfg,
-                cap0,           // dst
-                &adc_hw->fifo,  // src
-                CAPTURE_DEPTH,  // transfer count
-                true            // start immediately
+                cap0,          // dst
+                &adc_hw->fifo, // src
+                CAPTURE_DEPTH, // transfer count
+                true           // start immediately
             );
             dma_buffer = cap1;
         }
@@ -165,28 +155,28 @@ int main()
         for (size_t i = 0; i < CAPTURE_DEPTH; i++)
         {
             /* Convert the data to Q16 */
-            buffer[i] = ((int32_t)(dma_buffer[i])-(2048-60)) << 5; // 60 for resistor variance..
+            buffer[i] = ((int32_t)(dma_buffer[i]) - (2048 - 60)) << 5; // 60 for resistor variance..
 
 #ifndef NDEBUG
-            write_logger[log_index+i] = buffer[i];
+            write_logger[log_index + i] = buffer[i];
 #endif
         }
 #ifdef NDEBUG
         /* Call libmigic withe the processing buffer */
-        //libmigic_track(buffer, CAPTURE_DEPTH, &midiMsgs[0], midiBufferSize, &numberOfMessages, F16(0.1), F16(0.11), false, false, false);
+        // libmigic_track(buffer, CAPTURE_DEPTH, &midiMsgs[0], midiBufferSize, &numberOfMessages, F16(0.1), F16(0.11), false, false, false);
         for (size_t i = 0; i < numberOfMessages; i++)
         {
-            //tudi_midi_write24(0, midiMsgs[i].bytes[0], midiMsgs[i].bytes[1], midiMsgs[i].bytes[2]);
-            // if (midiMsgs[i].bytes[0] == 0x90)
-            // {
-            //     gpio_put(LED_PIN, 1);
-            //     gpio_put(BENCHMARK_PIN, 1);
-            // }
-            // else if (midiMsgs[i].bytes[0] == 0x80)
-            // {
-            //     gpio_put(LED_PIN, 0);
-            //     gpio_put(BENCHMARK_PIN, 0);
-            // }
+            // tudi_midi_write24(0, midiMsgs[i].bytes[0], midiMsgs[i].bytes[1], midiMsgs[i].bytes[2]);
+            //  if (midiMsgs[i].bytes[0] == 0x90)
+            //  {
+            //      gpio_put(LED_PIN, 1);
+            //      gpio_put(BENCHMARK_PIN, 1);
+            //  }
+            //  else if (midiMsgs[i].bytes[0] == 0x80)
+            //  {
+            //      gpio_put(LED_PIN, 0);
+            //      gpio_put(BENCHMARK_PIN, 0);
+            //  }
         }
 #endif
 
@@ -212,23 +202,41 @@ int main()
     return 0;
 }
 
-
 /* Core 1 currently only used for dumping data in the
  * debug build */
 void core1_main()
 {
-    static int32_t *last_read_logger = log1;
-    while(true)
+    while (true)
     {
-        printf("test\n");
-        sleep_ms(1000);
-        if (last_read_logger != read_logger)
+        if (tud_cdc_connected())
         {
-            last_read_logger = read_logger;
-            for (size_t i = 0; i < LOG_DEPTH; i++)
-            {
-                printf("%d\n", read_logger[i]);
-            }
+            tud_cdc_write_str("test\n");
+            tud_cdc_write_flush();
         }
+        tud_task();
+        sleep_ms(100); // Wait until USB CDC is connected
     }
+    // static int32_t *last_read_logger = log1;
+    // while (true)
+    // {
+    //     if (tud_cdc_connected())
+    //     {
+    //         tud_cdc_write_str("test\n");
+    //         tud_cdc_write_flush();
+    //         sleep_ms(100);
+
+    //         // if (last_read_logger != read_logger)
+    //         // {
+    //         //     last_read_logger = read_logger;
+    //         //     for (size_t i = 0; i < LOG_DEPTH; i++)
+    //         //     {
+    //         //         char buffer[32];
+    //         //         snprintf(buffer, sizeof(buffer), "%d\n", read_logger[i]);
+    //         //         tud_cdc_write_str(buffer);
+    //         //         tud_cdc_write_flush();
+    //         //     }
+    //         // }
+    //     }
+    //     tud_task(); // Handle TinyUSB background tasks
+    // }
 }
